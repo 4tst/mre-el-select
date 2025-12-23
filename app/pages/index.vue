732778d43c569b7ac6454ba0338ba6e0c4de393b
dlx <xdandyx@sina.cn>
2025-12-23 16:27:57 +0800

    test(select): fine

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,54 +1,45 @@
 <template>
-  <div class="relative">
-    <div class="absolute top-20px left-0 right-0 mx-auto color-white w-400px">
-      <div class="feature">
-        <el-tag type="primary" class="feature-name">unplugin-icons</el-tag
-        ><el-icon size="24" color="red"><LineMdIconify2Static /></el-icon>
-      </div>
-      <div class="feature">
-        <el-tag type="primary" class="feature-name">i18n</el-tag
-        >{{ t("hello") }}
-      </div>
-      <div class="feature">
-        <el-tag type="primary" class="feature-name" @contextmenu="onContextMenu"
-          >contextmenu</el-tag
-        >
-      </div>
-    </div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div>
+    <el-button type="danger" @click="clearOpts">清空选项</el-button>
+    <el-select v-model="opt" value-key="id" @change="handleChange">
+      <el-option
+        v-for="(item, i) in opts"
+        :key="i"
+        :label="item.label"
+        :value="item"
+      >
+        <div>
+          {{ item.label }}
+        </div>
+      </el-option>
+      <template #header>
+        <el-button size="small" type="primary">添加选项</el-button>
+      </template>
+      <template #empty>
+        <el-button size="small" type="primary">添加选项</el-button>
+      </template>
+    </el-select>
   </div>
 </template>
 
 <script setup lang="ts">
-import LineMdIconify2Static from "~icons/line-md/iconify2-static";
-import { useContextMenu } from "~/composables/context-menu";
-const { t } = useI18n();
-
-const onContextMenu = (e: MouseEvent) => {
-  useContextMenu(e, {
-    items: [
-      {
-        label: "menu",
-        icon: h(LineMdIconify2Static),
-        onClick: () => {
-          ElMessage.success({
-            message: "menu clicked",
-          });
-        },
-      },
-    ],
-  });
+const opts = ref([
+  {
+    id: 1,
+    label: "选项1",
+    payload: {
+      name: "xxx",
+      age: 18,
+    },
+  },
+]);
+const opt = ref<any>();
+const clearOpts = () => {
+  opts.value = [];
+};
+const handleChange = (val: any) => {
+  console.log(val);
 };
 </script>
 
-<style lang="scss" scoped>
-.feature {
-  display: flex;
-  align-items: center;
-  margin: 5px;
-  &-name {
-    width: 120px;
-  }
-}
-</style>
+<style scoped></style>
